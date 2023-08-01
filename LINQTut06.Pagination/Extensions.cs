e26aa1b9eeb0bc99d6540f2ee85cf852a2f2984b
slma445 <salmamhsen1@gmail.com>
2023-08-01 19:41:16 +0300

    Add project files.

---
+++ LINQTut06.Pagination/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTut06.Pagination
+{
+    public static class Extensions
+    {
+       public static IEnumerable <T> paginate <T> (this IEnumerable<T> source , int page =1 , int size =10)
+        {
+            if (page <= 0)
+                page = 1;
+            if (size <= 10)
+                size = 10;
+            var total = source.Count();
+            var pages = (int)Math.Ceiling((decimal)total / size);
+            var result = source.Skip((page - 1) * size).Take(size).ToList();
+            return result;
+
+        }
+    }
+}
---
+++ LINQTut06.Pagination/Program.cs
@@ -0,0 +1,41 @@
+﻿using LINQTut06.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace LINQTut06.Pagination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var page = 0;
+            var size = 10;
+
+            Console.WriteLine("result per page");
+            if(int.TryParse(Console.ReadLine(), out int resultPerPage))
+            {
+                size = resultPerPage;
+            }
+            Console.WriteLine("page no..:");
+            if(int.TryParse(Console.ReadLine(),out int pageNo))
+            {
+                page = pageNo;
+            }
+            var emps = Repository.LoadEmployees();
+            var result = emps.paginate(page, size);
+            var resultCount = result.Count();
+            var startRecord = ((page - 1) * size) + 1;
+            var endRecord = resultCount < size ? startRecord + resultCount - 1 : size * (page - 1);
+
+            result.Print($"showing employees {startRecord}{endRecord}");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
